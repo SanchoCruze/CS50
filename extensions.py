52895a0e44366c0902fb0682e1b894bfb914e0a8
Mawethu Madanile <127661983+SanchoCruze@users.noreply.github.com>
2025-07-19 10:22:15 +0200

    Add files via upload

---
+++ extensions.py
@@ -0,0 +1,19 @@
+answer = input("Ënter File Name: ").lower().strip()
+
+if answer.endswith (".gif"):
+        print("ïmage/gif")
+elif answer.endswith(".jpg"):
+        print("image/jpg")  
+elif answer.endswith(".jpeg"):
+        print("image/jpeg") 
+elif answer.endswith(".png"):
+        print("image/png")           
+elif answer.endswith(".pdf"):
+        print("application/pdf")  
+elif answer.endswith(".txt"):
+        print("text/txt")   
+elif answer.endswith(".zip"):
+        print("application/zip") 
+        
+
+
